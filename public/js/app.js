cf9158db4fb79bd39ddecfb966540d9868d12203
Deepak <deepakk.kumar@sunlife.com>
2020-04-20 19:01:07 +0530

    added console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 
-console.log('Client side js file isloaded ! ')
+console.log('Client side js file isloaded !')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
